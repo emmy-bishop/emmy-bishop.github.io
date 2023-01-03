ae11cf88fbcae8a1ad8cc39c71a82ead0ec80647
emmy-bishop <miaui96@gmail.com>
2023-01-03 21:31:32 +0000

    completed fizz-buzz

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -13,7 +13,27 @@
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
 
-
+    // loop through numbers 1-100
+    for (var i = 1; i <= 100; i++) {
+        // check if given number is divisible by both 3 and 5
+        if (i % 3 === 0 && i % 5 === 0) {
+            // if so log FizzBuzz
+          console.log('FizzBuzz');
+        }
+        // check if number is divisible by 3
+        else if (i % 3 === 0) {
+            // if so log Fizz
+          console.log('Fizz');
+        }
+        // check if number is divisible by 5
+         else if (i % 5 === 0) {
+            // if so log Buzz
+          console.log('Buzz');
+        } // otherwise log number
+         else {
+          console.log(i);
+        }
+      }
 
     // YOUR CODE GOES ABOVE HERE //
 }
