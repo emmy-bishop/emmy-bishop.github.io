cf68915edcf3067d7180405bbd0d5262ff2c99c4
emmy-bishop <miaui96@gmail.com>
2023-01-05 20:12:28 +0000

    studies

---
+++ studies/datatypes.js
@@ -0,0 +1,20 @@
+/**
+ * VARIABLES:
+ * 
+ * 0: (INTRO) Variables are named containers for data in JavaScript. We can declare variables with the var, let, or const keyword.
+ * (does not have to be exhaustive, just a basic intro, 3-4 sentences max)
+ * 
+ * 1: Declaration and Assignment: To create a variable in JavaScript, it must be declared with a declarative keyword.
+ * 
+ * 2: Var, Let, and Const: Var, let, and const are different declarative keywords and they do different things...
+ */
+
+// 1 Declaration and Assignment //
+
+var age; // declares variable
+age = 35; // assigns value to age variable
+
+// 2 Var, Let, and Const //
+
+// need at least 3 string methods
+// only need to pick a few unary operators
