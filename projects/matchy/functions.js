9d7ce5f9593b6f2d630a3b84d461a0dbd5a3868b
emmy-bishop <miaui96@gmail.com>
2023-01-10 21:46:35 +0000

    matchy

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -15,23 +15,23 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function search(animals, name) {
-    for (var i = 0; i < animals.length; i++) {
-      if (animals[i].name === name) {
-        return animals[i];
+function search(animals, name) { // create function w/ parameters representing animals array and name of animal to search
+    for (var i = 0; i < animals.length; i++) { // loop thru animals array
+      if (animals[i].name === name) { // check if name animal obj within animals array matches input name
+        return animals[i]; // if so return animal obj
       }
       }
-      return null;
+      return null; // otherwise return null
     }
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function replace(animals, name, replacement) {
-    for (var i = 0; i < animals.length; i++) {
-        if (animals[i].name === name) {
-            animals[i] = replacement;
+function replace(animals, name, replacement) { // create replace function
+    for (var i = 0; i < animals.length; i++) { // loop through animals array
+        if (animals[i].name === name) { // check if each animal's name matches input name
+            animals[i] = replacement; // if so replace animal obj w/ replacement obj
         } 
     }
 }
@@ -40,10 +40,10 @@ function replace(animals, name, replacement) {
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function remove(animals, name) {
-    for (var i = 0; i < animals.length; i++) {
-        if (animals[i].name === name) {
-            animals.splice(animals[i]);
+function remove(animals, name) { // create remove function
+    for (var i = 0; i < animals.length; i++) { // loop thru animals array
+        if (animals[i].name === name) { // check if given animal's name matches input name
+            animals.splice(animals[i]); // if so remove that animal obj
         }
     }
 }
@@ -52,15 +52,15 @@ function remove(animals, name) {
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-function add(animals, animal) {
-var matchExists = false;
-    for (var i = 0; i < animals.length; i++) {
-        if (animals[i].name === animal.name) {
-            matchExists = true;
+function add(animals, animal) { // create function to add animal
+var matchExists = false; // create variable to keep track of whether a match has been found
+    for (var i = 0; i < animals.length; i++) { // loop thru animals array
+        if (animals[i].name === animal.name) { // check if given animal's name matches name in input animal obj
+            matchExists = true; // if so change variable to true
         }
         }
-        if (matchExists === false) {
-            animals.push(animal);
+        if (matchExists === false) { // check if variable's value is false meaning no match was found
+            animals.push(animal); // if so push animal obj into animals array
         }
 }
 
