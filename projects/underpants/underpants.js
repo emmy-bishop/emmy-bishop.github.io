935873670aaadcb9958de8778597bb08a05cb8ab
emmy-bishop <miaui96@gmail.com>
2023-01-16 21:43:52 +0000

    underpants

--- projects/underpants/underpants.js
+++ projects/underpants/underpants.js
@@ -229,8 +229,11 @@ _.each = function(collection, func) {
 _.unique = function(array) {
     let newArray = [];
     for (let i = 0; i < array.length; i++) {
-        
+        if (array.indexOf(array[i]) === i) {
+            newArray.push(array[i]);
+        }
     }
+    return newArray;
 }
 
 
