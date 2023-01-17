29473ee598bb6edd8aaa289275b4edd556d3ff63
emmy-bishop <miaui96@gmail.com>
2023-01-17 23:12:10 +0000

    studies

--- studies/operators.js
+++ studies/operators.js
@@ -27,29 +27,56 @@
 
 var favoriteNumber = 5; // assignment operator assigns value of 5 to variable
 
-// 1. Addition Assignment Operators
+// 1. Addition Assignment Operator
 favoriteNumber += 1; // adds 1 to value of variable
 console.log(favoriteNumber); // prints => 6
 
-// 2. Subtraction Assignment Operators
+// 2. Subtraction Assignment Operator
 favoriteNumber -= 3; // subtracts 3 from variable
 console.log(favoriteNumber); // prints => 3
 
-// 3. Multiplication Assignment Operators
+// 3. Multiplication Assignment Operator
 favoriteNumber *= 4; // multiplies variable by 4
 console.log(favoriteNumber); // prints => 12
 
-// 4. Division Assignment Operators
+// 4. Division Assignment Operator
 favoriteNumber /= 2; // divides variable by 2
 console.log(favoriteNumber); // prints => 6
 
-// 5. Remainder Assignment Operators
+// 5. Remainder Assignment Operator
 favoriteNumber %= 1.5; // divides variable by 1.5 using remainder assignment operator. Assigns value of remainder
 console.log(favoriteNumber); // prints => 4
 
-// 6. Exponentiation Assignment Operators
+// 6. Exponentiation Assignment Operator
 favoriteNumber **= 2; // raises variable to the power of 2
 console.log(favoriteNumber); // prints => 16
 
 // 2 Arithmetic Operators //
+
+// 1. Addition Operator
+leastFavoriteNumber = 1 + 5; // adds numbers together
+console.log(leastFavoriteNumber); // prints => 6
+
+// 2. Subtraction Operator
+leastFavoriteNumber = 7 - 1; // subtracts numbers
+console.log(leastFavoriteNumber); // prints => 6
+
+// 3. Multiplication Operator
+leastFavoriteNumber = 3 * 2; // finds product of numbers
+console.log(leastFavoriteNumber); // prints => 6
+
+// 4. Division Operator
+leastFavoriteNumber = 12 / 2; // divides numbers
+console.log(leastFavoriteNumber); // prints => 6
+
+// 5. Remainder Operator
+leastFavoriteNumber = 22 % 8 // divides numbers and returns remainder 
+console.log(leastFavoriteNumber); // prints => 6
+
+// 6. Exponentiation Operator
+leastFavoriteNumber = 1.81712 ** 3 // raises number to the power of another number
+console.log(leastFavoriteNumber); // prints => 5.99999
+
+
+
 
