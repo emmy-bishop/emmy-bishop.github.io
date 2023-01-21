89bf9ac166d5d28f64331c7fb0fadcd64220afa7
emmy-bishop <miaui96@gmail.com>
2023-01-21 21:41:23 +0000

    studies

--- studies/datatypes.js
+++ studies/datatypes.js
@@ -5,7 +5,35 @@
  * Depending on the data type, each piece of data will have different properties and requirements.
  * 
  * 1: Number
+ * 
+ * 2: String
+ * 
+ * 3: Boolean
+ * 
+ * 4: Array
+ * 
+ * 5: Object
+ * 
+ * 6: Function
+ * 
+ * 7: Undefined
+ * 
+ * 8: null
+ * 
+ * 9: NaN
+ * 
+ * 10: Infinity and -Infinity
+ * 
+ * 11: Simple vs. Complex Data Types
+ * 
+ * 12: Copy by Value vs. Copy by Reference
  */
+
+// 1 Numbers //
+
+var squirrelCount = 5; // initializes variable to the value of a number
+console.log(squirrelCount) // prints => 5
+
 
 
 
