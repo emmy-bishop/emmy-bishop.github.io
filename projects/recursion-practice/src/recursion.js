e39f410a0993cca0b0cae8d9224a2c2c2d1110a8
emmy-bishop <miaui96@gmail.com>
2023-01-24 21:14:47 +0000

    recursion.js

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -126,8 +126,16 @@ var reverse = function(string) {
 };
 
 // 10. Write a function that determines if a string is a palindrome.
-var palindrome = function(string) {
-};
+var palindrome = function(string, newString = string.toLowerCase().replaceAll(' ', '')) { // create function with default parameter of newString, which is given string with no capital letters and all spaces removed
+  if (newString.length === 0) { // check if length of string is 0 meaning it is indeed a palindrome
+    return true; // if so return true
+  }
+  if (newString[0] === newString[newString.length - 1]) { // check if first and last letter of string equal each other
+    return palindrome(newString.slice(1, newString.length - 1)); // if so return result of invoking func on rest of string
+  }
+    return false; // otherwise return false
+  }
+
 
 // 11. Write a function that returns the remainder of x divided by y without using the
 // modulo (%) operator.
