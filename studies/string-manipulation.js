caf18b0eadc4a8c4a121a403236472282328706f
emmy-bishop <miaui96@gmail.com>
2023-01-28 23:06:27 +0000

    studies

---
+++ studies/string-manipulation.js
@@ -0,0 +1,21 @@
+/**
+ * STRING MANIPULATION:
+ * 
+ * 0: In JavaScript, it is possible to alter string values in various ways, even after they have been initialized.
+ * 
+ * 1: String Manipulation Using Operators: Strings can be concatenated, or added together, using operators.
+ * 
+ * 2: String Manipulation Using Methods: Strings can also be manipulated using built-in string methods.
+ */
+
+// 1 String Manipulation Using Operators //
+
+// 1. Concatenation Operator
+let monkeyString = 'Bonobo' + ' ' + '(Pan paniscus)'; // concatenates three strings
+console.log(monkeyString); // prints => Bonobo (Pan paniscus)
+
+// 2. Concatenation Assignment Operator
+monkeyString += ': Initially believed to be a subspecies of chimpanzee. Turns out that was super wrong.'; // adds to monkeyString
+console.log(monkeyString); // prints => Bonobo (Pan paniscus): Initially believed to be a subspecies of chimpanzee. Turns out that was super wrong.
+
+
