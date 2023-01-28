f2352c76494a1b1841fd1a9c8cdd915c5671d86f
emmy-bishop <miaui96@gmail.com>
2023-01-28 23:33:10 +0000

    studies

---
+++ studies/control-flow.js
@@ -0,0 +1,16 @@
+/**
+ * CONTROL FLOW:
+ * 
+ * 0: Control flow is the order by which the JavaScript interpreter parses code.
+ * In general, code is interpreted from top to bottom.
+ * However, certain factors can change the order in which code is read.
+ * 
+ * 1: If Statements: If statements delineate an action or actions to be performed IF the given statement evaluates to true.
+ * 
+ * 2: Else-if Statements: Delineate alternate action(s) to be performed only IF the first statement evaluates to false.
+ * 
+ * 3: Else Statements: Delineate actions to be performed only IF each and every preceding statement evaluates to false.
+ * 
+ * 4: Switch Statements: Delineate several possible actions to be executed depending on different conditions.
+ */
+
