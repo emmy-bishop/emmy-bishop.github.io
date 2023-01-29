5f0b4aa208ab13446810b82e4b08655b0a2ca229
emmy-bishop <miaui96@gmail.com>
2023-01-29 22:37:04 +0000

    studies

--- studies/loops.js
+++ studies/loops.js
@@ -1,5 +1,16 @@
 /**
  * LOOPS:
  * 
+ * 0: In programming, it is often necessary to repeat some action over and over.
+ * An easy way to accomplish this is with the use of a loop.
  * 
+ * 1: While, For, and For-In Loops: While loops will execute their code as long as a specified condition is true.
+ * In contrast, for loops execute their code as long as a specified condition is false.
+ * Finally, for-in loops are used to access and manipulate objects.
+ * 
+ * 2: Loop Any Number of Times: By changing the way a loop is set up, it is possible to loop any number of times and in either direction (forward or backward).
+ * 
+ * 3: Loop Over an Array: The for loop is the most common choice when looping over an array.
+ * 
+ * 4: Loop Over an Object: The for-in loop is the most common choice when looping over an object.
  */
