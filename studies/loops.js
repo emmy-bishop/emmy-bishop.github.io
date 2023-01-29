51cdf6544416ea471089e1efe1e24fc2bdb1b4fc
emmy-bishop <miaui96@gmail.com>
2023-01-29 22:51:44 +0000

    studies

--- studies/loops.js
+++ studies/loops.js
@@ -14,3 +14,28 @@
  * 
  * 4: Loop Over an Object: The for-in loop is the most common choice when looping over an object.
  */
+
+// 1 While, For, and For-In Loops //
+
+// 1. While Loop
+// While loops continue to execute the defined code as long as a certain condition remains true
+// Typically used when it is unknown how many times a loop will need to repeat
+let hour = 6; // establishes hour variable
+while (hour < 8) { // loops while hour is less than 8
+    console.log(`It is ${hour} and I am a sleepy little guy`); // executes while hour is 6 or 7
+    hour++; // increments counter at each iteration
+} // prints => It is 6 and I am a sleepy little guy 
+  //           It is 7 and I am a sleepy little guy
+
+// 2. For Loop
+// For loops continue to execute the defined code as long as a certain condition remains false
+// Typically used to iterate over arrays
+let seattlePosse = ['Frasier Crane', 'Niles Crane', 'Martin Crane', 'Roz Doyle', 'Daphne Moon'];
+for (let i = 0; i < seattlePosse.length; i++) {
+    
+}
+
+
+
+ 
+
