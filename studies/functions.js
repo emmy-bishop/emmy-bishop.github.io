796e2f22fdc25d8aa961dee596338e5a61ac6dea
emmy-bishop <miaui96@gmail.com>
2023-01-31 23:12:59 +0000

    studies

---
+++ studies/functions.js
@@ -0,0 +1,24 @@
+/**
+ * FUNCTIONS:
+ * 
+ * 0: In JavaScript, a function is a reusable block of code designed to perform a specialized task.
+ * After a function is declared, it can be called, or invoked, at any point to execute its code.
+ * Using functions makes it possible to streamline tasks that might otherwise be menial or repetitive.
+ * 
+ * 1: Declaration vs. Invocation: A function declaration gives a function a name and defines the actions it should execute.
+ * Post-declaration, any time we need to use a function, we invoke it.
+ * 
+ * 2: Parameters vs. Arguments: Parameters are defined in function declarations and act as placeholders for values that will be passed in.
+ * Arguments are defined at the time of the function invocation and represent the actual values to be used by the function.
+ * 
+ * 3: Named Function Syntax: Most functions are declared with names, and this must be done in a specific way.
+ * 
+ * 4: Functions Assigned to Variables: Functions are allowed to be nameless when assigned to variables.
+ * To invoke a function assigned to a variable, we refer to the name of the variable.
+ * 
+ * 5: Specifying Inputs and Outputs:
+ * 
+ * 6: Scope:
+ * 
+ * 7: Closures:
+ */
